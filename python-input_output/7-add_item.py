474b535a59f8851ebaa6629c6cd17f207a432c50
pdusenge <p.dusenge@alustudent.com>
2025-03-13 10:02:33 +0000

    loging

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -20,8 +20,8 @@ load_from_json_file_path = './6-load_from_json_file.py'
 save_to_json_file = load_module('save_to_json_file', save_to_json_file_path)
 load_from_json_file = load_module('load_from_json_file', load_from_json_file_path)
 my_list = []
-    
-
 my_list.extend(sys.argv[1:])
-
 save_to_json_file.save_to_json_file(my_list, filename)
+
+
+
