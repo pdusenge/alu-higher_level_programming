0b5549761af7ebbda0006b215789327bce789ea1
pdusenge <p.dusenge@alustudent.com>
2025-03-13 09:56:55 +0000

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -19,15 +19,10 @@ load_from_json_file_path = './6-load_from_json_file.py'
 # Dynamically load the modules
 save_to_json_file = load_module('save_to_json_file', save_to_json_file_path)
 load_from_json_file = load_module('load_from_json_file', load_from_json_file_path)
-
-if exists(filename):
-    my_list = load_from_json_file.load_from_json_file(filename)
-    print("Loaded list:", my_list)  # Debugging print
-else:
-    my_list = []
-    print("File not found, starting with empty list")  # Debugging print
+my_list = []
+    
 
 my_list.extend(sys.argv[1:])
-print("Updated list:", my_list)  # Debugging print
+print(my_list)  # Debugging print
 
 save_to_json_file.save_to_json_file(my_list, filename)
