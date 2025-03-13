158dd4b0c63681e4c6b835afe21276eab9772d53
pdusenge <p.dusenge@alustudent.com>
2025-03-13 10:05:48 +0000

    conditions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -19,7 +19,12 @@ load_from_json_file_path = './6-load_from_json_file.py'
 # Dynamically load the modules
 save_to_json_file = load_module('save_to_json_file', save_to_json_file_path)
 load_from_json_file = load_module('load_from_json_file', load_from_json_file_path)
-my_list = []
+
+if exists(filename):
+    my_list = load_from_json_file.load_from_json_file(filename)
+else:
+    my_list = []
+
 my_list.extend(sys.argv[1:])
 save_to_json_file.save_to_json_file(my_list, filename)
 
